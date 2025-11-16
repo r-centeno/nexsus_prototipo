447e4ec18d4aa6ef9b760b89756e2b0ab12248fe
Rodrigo Alexandre Evangelista Centeno <172276520+r-centeno@users.noreply.github.com>
2025-11-16 07:59:24 -0300

    Refactor event listener for DOMContentLoaded

--- frontend/script-code.js
+++ frontend/script-code.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const resposta = await fetch("http://127.0.0.1:5051/api/validar-codigo", {
+      const resposta = await fetch("/api/validar-codigo", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -59,5 +59,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
 
 

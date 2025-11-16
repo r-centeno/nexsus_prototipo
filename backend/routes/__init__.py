ab4b063f399713f1c438746a98d02ca10f918d27
Rodrigo Alexandre Evangelista Centeno <172276520+r-centeno@users.noreply.github.com>
2025-11-16 02:04:11 -0300

    Delete backend directory
    TROCA DE NOME

--- backend/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-from .registro import registro_bp
-
-all_blueprints = [registro_bp]
--- backend/routes/dashboard.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from utils.dashboard_data import obter_dados_dashboard
-
-dashboard_bp = Blueprint('dashboard', __name__)
-
-@dashboard_bp.route('/api/dashboard', methods=['GET'])
-def dashboard():
-    dados = obter_dados_dashboard()
-    return jsonify(dados)

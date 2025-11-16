ce437dc9f99680227df5b506535a7ce9f5d82a28
Rodrigo Alexandre Evangelista Centeno <172276520+r-centeno@users.noreply.github.com>
2025-11-16 04:53:31 -0300

    Change dashboard route to include full URL

--- api/routes/dashboard.py
+++ api/routes/dashboard.py
@@ -3,7 +3,8 @@ from utils.dashboard_data import obter_dados_dashboard
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
-@dashboard_bp.route('/api/dashboard', methods=['GET'])
+@dashboard_bp.route('http://127.0.0.1:5051/api/dashboard', methods=['GET'])
 def dashboard():
     dados = obter_dados_dashboard()
     return jsonify(dados)
+
